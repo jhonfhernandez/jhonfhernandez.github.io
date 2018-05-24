b7bc8fad41624c8b9cb0da151900c15e204671a7
jhonfhernandez <38089234+jhonfhernandez@users.noreply.github.com>
2018-05-23 21:54:58 -0500

    Add files via upload

---
+++ eventos.js
@@ -0,0 +1,36 @@
+var teclas = {
+  UP: 38,
+  DOWN: 40,
+  LEFT: 37,
+  RIGHT: 39
+};
+
+console.log(teclas);
+
+document.addEventListener("keyup", dibujarTeclado);
+
+function dibujarTeclado(evento)
+{
+  if(evento.keyCode == teclas.UP)
+  {
+    console.log("vamo pa arriba");
+  }
+  switch(evento.keyCode)
+  {
+    case teclas.UP:
+      console.log("arriba");
+    break;
+    case teclas.DOWN:
+      console.log("abajo");
+    break;
+    case teclas.RIGHT:
+      console.log("derecha");
+    break;
+    case teclas.LEFT:
+      console.log("izquierda");
+    break;
+
+    default:
+    console.log("otra tecla");
+  }
+}
